793c9e25593f907cf7b93fd3ee3d065df9ef52a6
Richard Widjaya <ricwidjaya@gmail.com>
2021-12-25 20:37:32 +0800

    feat: add home page

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,8 +1,21 @@
 const express = require("express")
 const router = express.Router()
+const db = require("../../models")
+const Todo = db.Todo
+const User = db.User
 
 router.get("/", (req, res) => {
-  res.send("Hello World")
+  Todo.findAll({
+    raw: true,
+    nest: true
+  })
+    .then((todos) => {
+      console.log(todos)
+      return res.render("index", { todos })
+    })
+    .catch((error) => {
+      return res.status(422).json(error)
+    })
 })
 
 module.exports = router
